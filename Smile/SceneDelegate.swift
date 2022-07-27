c7d7e4cc2969bb05240cbf17e975e166406f7117
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-27 19:05:13 +0530

    sceneDidEnterBackground: add saveContext()

--- Smile/SceneDelegate.swift
+++ Smile/SceneDelegate.swift
@@ -29,6 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+      (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
